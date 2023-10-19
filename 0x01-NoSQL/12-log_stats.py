6678bbf4823f7f57937ec39c4ff2ee257f9f31f0
codelord-evans <ekioko404@gmail.com>
2023-10-19 03:47:10 -0700

    task 12 correction

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
-"""a  Python script that provides some stats about Nginx
+"""Write a Python script that provides some stats about Nginx
 logs stored in MongoDB:
 
+Database: logs
+Collection: nginx
+Display (same as the example):
+first line: x logs where x is the number of documents in this collection
+second line: Methods:
+5 lines with the number of documents with the method =
+["GET", "POST", "PUT", "PATCH", "DELETE"] in this order
+(see example below - warning: it’s a tabulation before each line)
+one line with the number of documents with:
+method=GET
+path=/status
+You can use this dump as data sample: dump.zip
 """
 
 
